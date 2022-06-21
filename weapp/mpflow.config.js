45ce2bdf754dcfd0f0e25fe96769edf1f1293d07
ARCTURUS <1638330246@qq.com>
2022-06-21 22:26:02 +0800

    chore: 配置文件

---
+++ weapp/mpflow.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  appId: 'wx5015a94e3b5697be',
+  app: (mode) =>
+    mode === 'production' ? undefined : 'src/app',
+  pages: (mode) =>
+    mode === 'production'
+      ? ['src/components/index']
+      : undefined,
+  outputDir: 'miniprogram_dist',
+  compileType: 'miniprogram',
+  plugins: ['@mpflow/plugin-babel', '@mpflow/plugin-css'],
+  settings: {
+    urlCheck: true,
+    es6: false,
+    enhance: false,
+    postcss: false,
+    preloadBackgroundData: false,
+    minified: false,
+    newFeature: false,
+    coverView: true,
+    nodeModules: false,
+    autoAudits: false,
+    showShadowRootInWxmlPanel: true,
+    scopeDataCheck: false,
+    uglifyFileName: false,
+    checkInvalidKey: true,
+    checkSiteMap: true,
+    uploadWithSourceMap: true,
+    compileHotReLoad: false,
+    babelSetting: {
+      ignore: [],
+      disablePlugins: [],
+      outputPath: '',
+    },
+    useIsolateContext: true,
+    useCompilerModule: false,
+    userConfirmedUseCompilerModuleSwitch: false,
+  },
+};
